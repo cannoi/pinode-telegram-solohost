65fad8fe75129a4f759918d74e610f4c1f968099
cannoi <huunghitran.datxanh@gmail.com>
2026-08-24 13:03:01 +0700

    Update from ZIP

--- DataLive/Start-DataLive.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting DataLive HTTP API on http://127.0.0.1:18790/
-echo Require: PiNodeMonitorLive Service already writing latest.json
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0DataLive_HttpApi.ps1"
-pause
